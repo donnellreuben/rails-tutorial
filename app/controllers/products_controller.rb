93648f927da11aca9569a3d3c7e1a2c8b4926414
Donnell Reuben <donnell.reuben@icloud.com>
2023-03-31 02:27:43 -0500

    Fixed /one_product under def first needs @products not product.id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,19 @@
 class ProductsController < ApplicationController
-  
+  def every
+    @products = Product.all
+    render template: "recipes/index"
+  end
+
+  def first
+    @products = Product.first
+    render json: {
+      id: @products.id,
+      year: @products.year,
+      make: @products.make,
+      model: @products.model
+    }
+  end
+
   def index
     @products = Product.all
     # render json: @products
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do  
+  get "/all_products" => "products#every"
+  get "/one_product" => "products#first"
   get "/products" => "products#index"
 end
